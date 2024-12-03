d7c2f8092a7c4e0a7d79d18e7092a9bf2cf7deab
yesol <yeahsol.kwon@gmail.com>
2024-12-03 20:03:58 +1000

    Image slides fix for main

--- src/components/AutoPlay.jsx
+++ src/components/AutoPlay.jsx
@@ -1,9 +1,48 @@
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import "./AutoPlay.css";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 
-function AutoPlay() {
+const divStyle = {
+  display: "flex",
+  margin: "0 auto",
+  // alignItems: "center",
+  // justifyContent: "center",
+  width: "90%",
+  backgroundSize: "cover",
+  height: "600px",
+};
+
+const slideImages = [
+  {
+    url: "/assets/2.jpg",
+    caption: "Slide 1",
+  },
+  {
+    url: "/assets/4.jpg",
+    caption: "Slide 2",
+  },
+  {
+    url: "/assets/1.jpg",
+    caption: "Slide 3",
+  },
+];
+
+const Slideshow = () => {
+  return (
+    <div className="slide-container">
+      <Slide>
+        {slideImages.map((slideImage, index) => (
+          <div key={index}>
+            <div
+              style={{ ...divStyle, backgroundImage: `url(${slideImage.url})` }}
+            ></div>
+          </div>
+        ))}
+      </Slide>
+    </div>
+  );
+};
+/*function AutoPlay() {
   const settings = {
     dots: true,
     infinite: true,
@@ -36,5 +75,6 @@ function AutoPlay() {
     </div>
   );
 }
+*/
 
-export default AutoPlay;
+export default Slideshow;
